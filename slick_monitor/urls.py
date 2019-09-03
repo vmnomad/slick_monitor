4ebc206fe5cff8ba692857983611f8dd2af1818f
vmnomad <akopbayev@icloud.com>
2019-09-03 15:32:31 +1000

    fixed missing coma bug

--- slick_monitor/urls.py
+++ slick_monitor/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('setup/',include('setup.urls'))
+    path('setup/',include('setup.urls')),
     path('monitors/',include('monitors.urls')),
     path('admin/', admin.site.urls)
 ]
