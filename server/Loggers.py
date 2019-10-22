678ebbb4b095af9cb0a811e8d9ad379d2285c086
vmnomad <akopbayev@icloud.com>
2019-10-23 10:04:38 +1100

    ALlow users to modify handler logging

--- server/Loggers.py
+++ server/Loggers.py
@@ -16,6 +16,7 @@ class Netcat_handler(logging.Handler):
             self.port = port          
             self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.socket.connect((self.hostname, int(self.port)))
+           
             self.init = True
         except Exception as error:
             self.init = False
@@ -23,8 +24,16 @@ class Netcat_handler(logging.Handler):
     def __repr__(self):
         return 'Netcat Handler: {}, {}'.format(self.hostname, self.port)
 
-    def update(self, settings):
-        pass
+    def update(self, hostname, port, logging_level):
+        if self.hostname != hostname or \
+           self.port != port:
+            self.socket.close
+            self.hostname = hostname
+            self.port = port          
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((self.hostname, int(self.port)))
+        self.setLevel(logging_level)
+
 
 
     def emit(self, record):
@@ -35,7 +44,7 @@ class Netcat_handler(logging.Handler):
             except Exception as error:
                 print('Failed emit. Error:', error)
         else:
-            print('Nc_handler is not initialised.')
+            print('Netcat_handler is not initialised.')
 
 def get_logging_config():
 
@@ -74,41 +83,32 @@ def get_netcat_handler(settings):
     return nc_handler
 
 def get_logging_handler(log_type, settings):
-    if log_type == 'console' and settings['enabled'] == 1:
-        return get_console_handler(settings)
-    elif log_type == 'file' and settings['enabled'] == 1:
-        return get_file_handler(settings)
-    elif log_type == 'netcat' and settings['enabled'] == 1:
-        return get_netcat_handler(settings)
-    else:
-        logging.info('Cannot find logging handler type: {}'.format(log_type))
+    try:
+        if log_type == 'console' and settings['enabled'] == 1:
+            return get_console_handler(settings)
+        elif log_type == 'file' and settings['enabled'] == 1:
+            return get_file_handler(settings)
+        elif log_type == 'netcat' and settings['enabled'] == 1:
+            return get_netcat_handler(settings)
+        else:
+            logging.info('Cannot find logging handler type: {}'.format(log_type))
+            return None
+    except:
         return None
 
-def update_logging_handler(log_type, settings):
-    if log_type == 'console' and settings['enabled'] == 1:
-        return get_console_handler(settings)
-    elif log_type == 'file' and settings['enabled'] == 1:
-        return get_file_handler(settings)
-    elif log_type == 'netcat' and settings['enabled'] == 1:
-        return get_netcat_handler(settings)
-    else:
-        logging.info('Cannot find logging handler type: {}'.format(log_type))
-        return None
 
 def get_logger():
     logger = logging.getLogger(__name__)
     logger.propagate = False # disables propagation of mylogger to root logger
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
 
     try:
         logging_config = get_logging_config()
         for log_type, settings in logging_config.items():
-            print('Logger: {}, Enabled: {}'.format(log_type, settings['enabled']))
-
             log_handler = get_logging_handler(log_type, settings)
-
             if log_handler != None:
                 logger.addHandler(log_handler)
+                logger.info('{} logging is enabled'.format(log_type.capitalize()))
             
     except Exception as error:
         print('Failed to initialise logging handler {}. Error: {}'.format(log_type, error))
@@ -117,5 +117,17 @@ def get_logger():
 
 def update_logger(logger):
     handlers = logger.handlers
+    new_config = logging_config = get_logging_config()
     for i in range(len(handlers)):
-        print(handlers[i].name)
+        #print(handlers[i].name)
+        h_name = handlers[i].name
+        if h_name != 'netcat_handler':
+            handlers[i].setLevel = new_config[h_name.split('_')[0]]['logging_level']
+            #print('Setting Handler: {} with new logging level: {}'.format(h_name, new_config[h_name.split('_')[0]]['logging_level']))
+        else:
+            #print(new_log_level)
+            new_hostname = new_config[h_name.split('_')[0]]['hostname']
+            new_port = new_config[h_name.split('_')[0]]['port']
+            new_logging_level = new_config[h_name.split('_')[0]]['logging_level']
+            handlers[i].update(new_hostname, new_port, new_logging_level)
+            print('Setting Handler: {} with new logging level: {}'.format(h_name, new_config[h_name.split('_')[0]]['logging_level']))
