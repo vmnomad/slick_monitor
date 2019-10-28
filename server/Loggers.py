b4c84feade2ad3d189ee1c6a7a4452f04f17b7e6
vmnomad <akopbayev@icloud.com>
2019-10-29 07:56:23 +1100

    Allow users to enable/disable logging handlers
    New functions were added to remove/add handlers in QueueListener
    logger.

--- server/Loggers.py
+++ server/Loggers.py
@@ -16,7 +16,7 @@ LOGGING_FORMAT = logging.Formatter(' %(asctime)s - %(levelname)s - %(message)s -
 # setting up local logger
 loggers_logger = logging.getLogger(__name__)
 loggers_logger.propagate = False
-loggers_logger.setLevel(logging.DEBUG)
+loggers_logger.setLevel(logging.INFO)
 s_handler = logging.StreamHandler()
 s_handler.setFormatter(COLOR_LOGGING_FORMAT)
 s_handler.setLevel(logging.DEBUG)
@@ -36,9 +36,14 @@ class Netcat_handler(logging.Handler):
             loggers_logger.error('Failed to initialized Nc_handler. Error: {}'.format(error))
 
     def nc_connect(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((self.hostname, int(self.port)))
-        self.init = True
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((self.hostname, int(self.port)))
+            self.init = True
+        except Exception as error:
+            self.init = True
+            loggers_logger.error('Failed to connect to {} on port {}. Error: {}'.format(self.hostname, self.port, error))
+
 
     def __repr__(self):
         return 'Netcat Handler: {}, {}'.format(self.hostname, self.port)
@@ -62,6 +67,7 @@ class Netcat_handler(logging.Handler):
                 self.socket.send((log_entry + '\n').encode())
             except BrokenPipeError as error:
                 self.nc_connect()
+                #self.socket.send((log_entry + '\n').encode())
                 loggers_logger.error('Failed to send log to netcat host. Please ensure netcat is configured on host {} , port {}. Error: {}'.format(self.hostname, self.port, error))
             except Exception as error:
                 loggers_logger.error('Failed emit. Error: {}'.format(error))
@@ -70,7 +76,7 @@ class Netcat_handler(logging.Handler):
 
 def get_logging_config():
 
-    conn = sqlite3.connect('server test.db', timeout=5.0)
+    conn = sqlite3.connect('server.db', timeout=5.0)
     conn.row_factory = sqlite3.Row
     cursor = conn.execute("SELECT * from LOGGERS")
     result = cursor.fetchall()
@@ -156,16 +162,64 @@ def get_logger():
 
     return logger
 
+## Update Handlers code
+def get_handler_index(logger, h_name):
+    for h in logger.handlers:
+        if h.name == h_name:
+            return logger.handlers.index(h)
+    return -1
+
+def remove_handler(handlers, h_index):
+    temp_handlers = list(handlers)
+    temp_handlers.pop(h_index)
+    return tuple(temp_handlers)
+
+def add_handler(handlers, h_name, h_settings):
+    h_name = h_name.split('_')[0]
+    log_handler = get_logging_handler(h_name, h_settings)
+    if log_handler != None:
+        temp_handlers = list(handlers)
+        temp_handlers.append(log_handler)
+        return tuple(temp_handlers)
+
+    return handlers
+
+
+def update_handlers(logger):
+    configuration = get_logging_config()
+    #print(configuration.keys())
+    for h_config_name, h_config_settings in configuration.items():
+    
+        h_name = h_config_name + '_handler'
+
+        # deleting handlers
+        if h_config_settings['enabled'] == 0:
+            h_index = get_handler_index(logger, h_name)
+            if h_index != -1:
+                logger.handlers = remove_handler(logger.handlers, h_index)
+                loggers_logger.info('Handler {} was disabled'.format(h_name))
+
+        if h_config_settings['enabled'] == 1:
+            h_index = get_handler_index(logger, h_name)
+            if h_index == -1:
+                logger.handlers = add_handler(logger.handlers, h_name, h_config_settings)
+                loggers_logger.info('Handler {} was enabled'.format(h_name))
+
+
 # temp disabled
 def update_logger(logger):
     handlers = logger.handlers
     new_config = logging_config = get_logging_config()
     for i in range(len(handlers)):
         h_name = handlers[i].name
+
+        h_config = new_config[h_name.split('_')[0]]
+
         if h_name != 'netcat_handler':
             handlers[i].setLevel = new_config[h_name.split('_')[0]]['logging_level']
             loggers_logger.debug('Setting Handler: {} with new logging level: {}'.format(h_name, new_config[h_name.split('_')[0]]['logging_level']))
         else:
+
             new_hostname = new_config[h_name.split('_')[0]]['hostname']
             new_port = new_config[h_name.split('_')[0]]['port']
             new_logging_level = new_config[h_name.split('_')[0]]['logging_level']
